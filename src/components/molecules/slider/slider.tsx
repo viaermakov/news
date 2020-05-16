728c6f7340dc0095564de4c04c401fa758e88370
Viacheslav Ermakov <slavaskainet@gmail.com>
2020-05-17 00:24:18 +0300

    added closing slider on outside click

--- src/components/molecules/slider/slider.tsx
+++ src/components/molecules/slider/slider.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import { observer } from 'mobx-react-lite';
-import cls from 'classnames';
 import { CSSTransition } from 'react-transition-group';
 import styles from './slider.scss';
 
@@ -10,25 +9,32 @@ interface ISliderComponentProps {
 }
 
 const Slider: React.FC<ISliderComponentProps> = ({ children, isOpen = false }) => {
-  const [isVisible, setIsVisible] = React.useState<boolean>(isOpen);
-  const [showButton, setShowButton] = React.useState<boolean>(false);
+  const [isVisible, setIsVisible] = React.useState<boolean>(false);
+  const sliderRef = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (isOpen && !sliderRef?.current?.contains(event.target as Node)) {
+        handleToggle();
+      }
+    }
+
+    window.addEventListener('click', handleClickOutside);
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
 
   React.useEffect(() => {
     setIsVisible(isOpen);
   }, [isOpen]);
 
-  const handleShow = () => {
+  const handleToggle = () => {
     setIsVisible(state => !state);
   };
 
-  const renderShowButton = () => (
-    <span className={cls(styles.cc, { [styles.hidden]: !showButton })} onClick={handleShow}>
-      Show
-    </span>
-  );
-
   return (
-    <div className={styles.back}>
+    <div className={styles.back} ref={sliderRef}>
       <CSSTransition
         in={isVisible}
         timeout={200}
@@ -36,17 +42,14 @@ const Slider: React.FC<ISliderComponentProps> = ({ children, isOpen = false }) =
         classNames={{
           ...styles,
         }}
-        onExited={() => setShowButton(true)}
-        onEnter={() => setShowButton(false)}
       >
         <section className={styles.wrapper}>
-          <span className={styles.close} onClick={handleShow}>
+          <span className={styles.close} onClick={handleToggle}>
             ×
           </span>
           {children}
         </section>
       </CSSTransition>
-      {renderShowButton()}
     </div>
   );
 };
